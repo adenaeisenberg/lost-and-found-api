7de94c7acf066c08ebed88ea628c18518c976801
Adena <adenaeisenberg@gmail.com>
2024-02-20 16:33:12 -0500

    adds routes for all CRUD actions for FoundItems

---
+++ app/controllers/found_items_controller.rb
@@ -0,0 +1,2 @@
+class FoundItemsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  # Lost Item Routes
   get "/lost_items" => "lost_items#index"
   post "/lost_items" => "lost_items#create"
   get "/lost_items/:id" => "lost_items#show"
   patch "/lost_items/:id" => "lost_items#update"
   delete "/lost_items/:id" => "lost_items#destroy"
 
+  # Found Items Routes
+  get "/found_items" => "found_items#index"
+  post "/found_items" => "found_items#create"
+  get "/found_items/:id" => "found_items#show"
+  patch "/found_items/:id" => "found_items#update"
+  delete "/found_items/:id" => "found_items#destroy"
 end
