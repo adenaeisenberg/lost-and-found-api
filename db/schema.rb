c04f2823ca4c61453d73ca4257bf1483bf129be9
Adena <adenaeisenberg@gmail.com>
2024-02-20 15:17:58 -0500

    adds FoundItem table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_20_200212) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_20_201651) do
+  create_table "found_items", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.string "location"
+    t.string "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "lost_items", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
