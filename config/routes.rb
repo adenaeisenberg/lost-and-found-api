a72387f6ab1f9f7e4c059b16c8401f9e499dc2d9
Adena <adenaeisenberg@gmail.com>
2024-02-20 16:51:26 -0500

    adds comment in routes:

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  # Lost Item Routes
+  # Lost Item Routes (all have been tested and work)
   get "/lost_items" => "lost_items#index"
   post "/lost_items" => "lost_items#create"
   get "/lost_items/:id" => "lost_items#show"
   patch "/lost_items/:id" => "lost_items#update"
   delete "/lost_items/:id" => "lost_items#destroy"
 
-  # Found Items Routes
+  # Found Items Routes (all have been tested and work)
   get "/found_items" => "found_items#index"
   post "/found_items" => "found_items#create"
   get "/found_items/:id" => "found_items#show"
