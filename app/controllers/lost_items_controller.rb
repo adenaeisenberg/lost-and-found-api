8c340d83c531b5aa10aae6fd76c6e1c35438c761
Adena <adenaeisenberg@gmail.com>
2024-02-20 16:18:43 -0500

    adds update action

--- app/controllers/lost_items_controller.rb
+++ app/controllers/lost_items_controller.rb
@@ -19,4 +19,14 @@ class LostItemsController < ApplicationController
     render :show
   end
 
+  def update
+    @lost_item = LostItem.find_by(id: params[:id])
+    @lost_item.update(
+      name: params[:name] || @lost_item.name,
+      image_url: params[:image_url] || @lost_item.image_url,
+      last_seen: params[:last_seen] || @lost_item.last_seen,
+    )
+    render :show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "/lost_items" => "lost_items#index"
   post "/lost_items" => "lost_items#create"
   get "/lost_items/:id" => "lost_items#show"
+  patch "/lost_items/:id" => "lost_items#update"
 
 end
